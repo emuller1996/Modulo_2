5f196ccd17dc0b1bffc6b028b4e3b8ab2bce2f0f
emuller1996 <stefano.muller@hotmail.com>
2022-06-20 22:13:05 -0500

    Finalizado Intro React

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -2,5 +2,20 @@ import React from 'react';
 
 export default function Card(props) {
   // acá va tu código
-  return <div>Card Component</div>
+  return (
+    <div>
+      <button onClick={props.onClose} >X</button>
+      <h4>{props.name}</h4>
+      <div>
+        <span>Min  </span>
+        <span>{props.min} {props.max}</span>
+      </div>
+      <div>
+        <span>Max  </span>
+        <span>{props.max}</span>
+      </div>
+      <img src={`http://openweathermap.org/img/wn/${props.img}@2x.png`} alt="IMG_DSDS" />
+    </div>
+
+  )
 };
--- 06-React-Intro/homework/src/components/Cards.jsx
+++ 06-React-Intro/homework/src/components/Cards.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import Card from './Card.jsx';
 
 export default function Cards(props) {
   // acá va tu código
   // tip, podés usar un map
-  return <div>Cards Component</div>
+  const cards = props.cities.map((card) =>
+    <Card
+      key={card.id}
+      max={card.main.temp_max}
+      min={card.main.temp_min}
+      name={card.name}
+      img={card.weather[0].icon}
+      onClose={() => alert(card.name)}
+    />
+  );
+  return (
+    <div>
+      {cards}
+    </div>)
 };
